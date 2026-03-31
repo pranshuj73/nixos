0ea38c598e6bf61cfeec1a8ab0ab98078dc196cc
prnsh <58560660+pranshuj73@users.noreply.github.com>
2026-03-31 21:10:42 +0530

    cleanup handy config

--- hosts/zerogoki/home.nix
+++ hosts/zerogoki/home.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, inputs, ... }:
+{ config, pkgs, inputs, lib, ... }:
 
 let
   dotfiles = "/home/prnsh/dotfiles";
@@ -100,31 +100,6 @@ in
 
 
   services.handy.enable = true;
-  options.services.handy = {
-    enable = lib.mkEnableOption "Handy speech-to-text user service";
-
-    package = lib.mkOption {
-      type = lib.types.package;
-      defaultText = lib.literalExpression "handy.packages.\${system}.handy";
-      description = "The Handy package to use.";
-    };
-  };
-
-  config = lib.mkIf cfg.enable {
-    systemd.user.services.handy = {
-      Unit = {
-        Description = "Handy speech-to-text";
-        After = [ "graphical-session.target" ];
-        PartOf = [ "graphical-session.target" ];
-      };
-      Service = {
-        ExecStart = "${cfg.package}/bin/handy";
-        Restart = "on-failure";
-        RestartSec = 5;
-      };
-      Install.WantedBy = [ "graphical-session.target" ];
-    };
-  };
 
   programs.zsh = {
     enable = true;
